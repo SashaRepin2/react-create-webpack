d6114b0d4448f4e6266633ea65f4f1be556c99f5
Sasha Repin <sasharepin2016@gmail.com>
2022-08-16 19:16:46 +0300

    some changes

--- src/components/ListsGroup/List/Task/Task.tsx
+++ src/components/ListsGroup/List/Task/Task.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import { ITask, Statuses } from "../../../../interfaces/ITask";
